68e99b56128df472e17e9ec2e38ecde986d4f091
Perry Brandiezs <pbrandiezs@gmail.com>
2019-12-05 09:10:53 -0800

    feat: Add exception handler to UserController class

--- src/main/java/com/example/springbootexception/controller/UserController.java
+++ src/main/java/com/example/springbootexception/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.springbootexception.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -10,5 +12,13 @@ public class UserController {
     public String add() {
         int num = 10 / 0;
         return "add";
+    }
+
+    @ExceptionHandler(value={java.lang.ArithmeticException.class})
+    public ModelAndView handlerArithmeticException(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e.toString());
+        modelAndView.setViewName("mathError");
+        return modelAndView;
     }
 }
